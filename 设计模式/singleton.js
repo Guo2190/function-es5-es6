a42cda6518443399b2c02e4bcdbf8784565d20d9
snow <snow@example.com>
2019-06-18 17:56:28 +0800

    单例模式

--- 设计模式/singleton.js
+++ 设计模式/singleton.js
@@ -12,3 +12,28 @@ var singleton = function( fn ){
 var createMask = singleton( function(){
     return document.body.appendChild( document.createElement('div') );
 })
+
+class  SingletonApple {
+    constructor(name, creator, products) {
+        // first
+        this.name = name;
+        this.creator = creator;
+        this.products = products;
+       
+    }
+    //静态方法
+    static getInstance(name, creator, products) {
+        console.log(this)
+        if(!this.instance) {
+            this.instance = new SingletonApple(name, creator, products);
+        }
+        return this.instance;
+    }
+}
+
+
+    let appleCompany = SingletonApple.getInstance('苹果公司', '乔布斯', ['iPhone', 'iMac', 'iPad', 'iPod']);
+    let copyApple = SingletonApple.getInstance('苹果公司', '阿辉', ['iPhone', 'iMac', 'iPad', 'iPod'])
+    
+    console.log(appleCompany === copyApple); //true
+
