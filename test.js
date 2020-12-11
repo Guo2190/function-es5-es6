c497447848d00ada3fb0cb792a6cab3d3ab1d00f
Gxf <gxf920206@gmail.com>
2020-12-11 10:12:15 +0800

    fix: n

--- test.js
+++ test.js
@@ -1,10 +1,28 @@
-var a = 10;
-var o = {
-    a: 11,
-    b: {
-        fn: function() {
-            console.log(this.a);
+var threeSum = function(nums) {
+    const arr = []
+    let l = 0
+    let r = nums.length - 1
+    while (l < r) {
+        let add = nums[l] + nums[r]
+        let index = nums.findIndex((e, i) => {
+            if (i != l && i != r) {
+                return (add - e) === 0
+            }
+        })
+        if (index !== -1) {
+            console.log(l, r, index)
+            arr.push([nums[l], nums[r], nums[index]])
+                ++l
+                --r
+        } else {
+            if (nums[l] > nums[r]) {
+                --r
+            } else {
+                ++l
+            }
         }
     }
-}
-o.b.fn();
+    return arr
+};
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4]))
