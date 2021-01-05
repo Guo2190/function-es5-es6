9bd2fdc50342c333ab5dc1492d8d66644783aac3
Gxf <gxf920206@gmail.com>
2021-01-05 17:50:00 +0800

    fix： 动态规划 正则回文

--- leetcode/10RegularExpressionMatching.js
+++ leetcode/10RegularExpressionMatching.js
@@ -1,3 +1,15 @@
 var isMatch = function(s, p) {
-    while ()
+    let i = 0
+    let j = 0
+    while (j < p.length - 1) {
+        if (i >= s.length - 1) {
+            return false
+        }
+        if (s[i++] != p[j++]) {
+            return false
+        }
+    }
+    return j === s.length - 1
 };
+
+console.log(isMatch('sdaaaaa', 'sd...aa'))
