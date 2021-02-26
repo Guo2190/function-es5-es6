db0092cf34de53dd42d2f83d48868a43365bad1f
Gxf <gxf920206@gmail.com>
2021-02-26 17:31:50 +0800

    fix: 134

--- leetcode/贪心算法/134.js
+++ leetcode/贪心算法/134.js
@@ -4,11 +4,20 @@
  * @return {number}
  */
 var canCompleteCircuit = function(gas, cost) {
-  let tank = 0
-  let circle = 0
-  for(let i = 0; i<cost.length;i++){
-    
+  let sum = 0
+  let curSum = 0
+  let res = 0
+
+  for(let circle = 0;circle<cost.length;circle++){
+    sum +=gas[circle]-cost[circle]
+    curSum +=gas[circle]-cost[circle]
+    if(curSum<0){
+      res=circle+1
+      curSum = 0
+    }
   }
+ return sum<0?-1:res
+  
 };
 
-console.log(canCompleteCircuit([1,2,3,4,5],[3,4,5,1,2]))
+console.log(canCompleteCircuit([1,2,3,4,5],[3,1,5,1,2]))
