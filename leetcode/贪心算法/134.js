dcb9e6c2d90f9923043ff86b14a4fcaaa0b2cad2
Gxf <gxf920206@gmail.com>
2021-03-02 11:26:33 +0800

    fix: 134

--- leetcode/贪心算法/134.js
+++ leetcode/贪心算法/134.js
@@ -2,22 +2,32 @@
  * @param {number[]} gas
  * @param {number[]} cost
  * @return {number}
+ * leetcode 134题 
+ * 汽车作为起点的条件： 当前站点汽油值大于消耗值 则可驶向下一个站点 并记录上一个站点剩余的汽油和当前站点消耗和存储的汽油质量  若小于0 则说明当前i 不适合
+ * 作为起点， 则把七点设置为下一个点 继续遍历
+ * 并记录所有站点总汽油量是否大于消耗量   如果大于等于 且有站点汽油值大于消耗值 说明可以环绕一周 
+ *
+ * 
+ * 
+ * 
+ * 
  */
 var canCompleteCircuit = function(gas, cost) {
   let sum = 0
   let curSum = 0
   let res = 0
-
-  for(let circle = 0;circle<cost.length;circle++){
-    sum +=gas[circle]-cost[circle]
-    curSum +=gas[circle]-cost[circle]
+  for(let i = 0; i < gas.length; i++){
+    sum += gas[i]-cost[i]
+    curSum += gas[i]-cost[i]
     if(curSum<0){
-      res=circle+1
-      curSum = 0
+      res=i+1
+      curSum=0
     }
   }
- return sum<0?-1:res
+  console.log(sum)
+  return sum>=0?res:-1
   
 };
 
-console.log(canCompleteCircuit([1,2,3,4,5],[3,1,5,1,2]))
+console.log(canCompleteCircuit([3,1,1],
+  [1,2,2]))
